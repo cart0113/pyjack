7b655379286ededd9b4d6db259c57af3deadc1dc
Josh Junon <josh@junon.me>
2017-05-12 04:19:58 -0700

    python 3 support

--- pyjack.py
+++ pyjack.py
@@ -3,8 +3,8 @@ pyjack
 ******
 
 pyjack is debug/test/monkey-patching toolset that allows you to reversibly
-replace all references to a function /object in memory with a 
-proxy function/object.  
+replace all references to a function /object in memory with a
+proxy function/object.
 
 :copyright: Copyright 2009-2012 by Andrew Carter <andrewjcarter@gmail.com>
 :license: MIT (http://www.opensource.org/licenses/mit-license.php)
@@ -21,32 +21,32 @@ _WRAPPER_TYPES = (type(object.__init__), type(object().__init__),)
 def proxy0(data):
     def proxy1(): return data
     return proxy1
-_CELLTYPE = type(proxy0(None).func_closure[0])
+_CELLTYPE = type(proxy0(None).__closure__[0])
 
 
 class PyjackException(Exception): pass
 
 
-def connect(fn, proxyfn):    
+def connect(fn, proxyfn):
     """\
     :summary: Connects a filter/callback function to a function/method.
-                      
+
     :param fn: The function which to pyjack.
-    :type fn:  :class:`types.FunctionType`, :class:`types.MethodType`, 
-               :class:`types.BuiltinFunctionType`, :class:`BuiltinMethodType` 
+    :type fn:  :class:`types.FunctionType`, :class:`types.MethodType`,
+               :class:`types.BuiltinFunctionType`, :class:`BuiltinMethodType`
                or any callable that implements :func:`__call__`
-        
+
     :param proxyfn: Any callable.  It will be passed the original :attr:`fn`
-                    and then any args, kwargs that were passed to the 
-                    original :attr:`fn`. 
-    :type proxyfn:  callable. 
-        
+                    and then any args, kwargs that were passed to the
+                    original :attr:`fn`.
+    :type proxyfn:  callable.
+
     :returns: The new pyjacked function.  Note, this function object
               has a :func:`restore` that can be called to remove the
-              pyjack filters/callbacks. 
-                      
+              pyjack filters/callbacks.
+
     :raises: :class:`PyjackException`
-    """    
+    """
     fn_type = type(fn)
     if issubclass(fn_type, _types.FunctionType):
         return _PyjackFuncCode(fn, proxyfn)._fn
@@ -71,19 +71,19 @@ def connect(fn, proxyfn):
     else:
         bundle = (fn, fn_type,)
         raise PyjackException("fn %r of type '%r' not supported" % bundle)
-    
+
 
 def restore(fn):
     """\
-    :summary:  Fully restores function back to original state. 
-                      
-    :param fn: The pyjacked function returned by :func:`connect`. 
-    
+    :summary:  Fully restores function back to original state.
+
+    :param fn: The pyjacked function returned by :func:`connect`.
+
     .. note::
-    
-       Any pyjacked function has a :func:`restore` method, too.  So you can 
-       call that instead of this procedural function -- it's up to you.     
-        
+
+       Any pyjacked function has a :func:`restore` method, too.  So you can
+       call that instead of this procedural function -- it's up to you.
+
     """
     fn.restore()
 
@@ -94,27 +94,27 @@ def restore(fn):
 def replace_all_refs(org_obj, new_obj):
     """
     :summary: Uses the :mod:`gc` module to replace all references to obj
-              :attr:`org_obj` with :attr:`new_obj` (it tries it's best, 
-              anyway). 
-                      
-    :param org_obj: The obj you want to replace. 
-    
-    :param new_obj: The new_obj you want in place of the old obj. 
-    
+              :attr:`org_obj` with :attr:`new_obj` (it tries it's best,
+              anyway).
+
+    :param org_obj: The obj you want to replace.
+
+    :param new_obj: The new_obj you want in place of the old obj.
+
     :returns: The org_obj
-    
+
     Use looks like:
-    
+
     >>> import pyjack
     >>> x = ('org', 1, 2, 3)
     >>> y = x
     >>> z = ('new', -1, -2, -3)
     >>> org_x = pyjack.replace_all_refs(x, z)
     >>> print x
-    ('new', -1, -2, -3)    
-    >>> print y 
-    ('new', -1, -2, -3)    
-    >>> print org_x 
+    ('new', -1, -2, -3)
+    >>> print y
+    ('new', -1, -2, -3)
+    >>> print org_x
     ('org', 1, 2, 3)
 
     To reverse the process, do something like this:
@@ -123,35 +123,35 @@ def replace_all_refs(org_obj, new_obj):
     >>> del org_x
     >>> print x
     ('org', 1, 2, 3)
-    >>> print y 
+    >>> print y
     ('org', 1, 2, 3)
     >>> print z
-    ('new', -1, -2, -3)    
-        
+    ('new', -1, -2, -3)
+
     .. note:
-        The obj returned is, by the way, the last copy of :attr:`org_obj` in 
-        memory; if you don't save a copy, there is no way to put state of the 
-        system back to original state.     
-    
-    .. warning:: 
-       
-       This function does not work reliably on strings, due to how the 
-       Python runtime interns strings. 
-        
+        The obj returned is, by the way, the last copy of :attr:`org_obj` in
+        memory; if you don't save a copy, there is no way to put state of the
+        system back to original state.
+
+    .. warning::
+
+       This function does not work reliably on strings, due to how the
+       Python runtime interns strings.
+
     """
 
     _gc.collect()
-    
-    hit = False    
+
+    hit = False
     for referrer in _gc.get_referrers(org_obj):
-                
+
         # FRAMES -- PASS THEM UP
         if isinstance(referrer, _types.FrameType):
             continue
 
         # DICTS
         if isinstance(referrer, dict):
-             
+
             cls = None
 
             # THIS CODE HERE IS TO DEAL WITH DICTPROXY TYPES
@@ -159,7 +159,7 @@ def replace_all_refs(org_obj, new_obj):
                 for cls in _gc.get_referrers(referrer):
                     if _inspect.isclass(cls) and cls.__dict__ == referrer:
                         break
-            
+
             for key, value in referrer.items():
                 # REMEMBER TO REPLACE VALUES ...
                 if value is org_obj:
@@ -173,14 +173,14 @@ def replace_all_refs(org_obj, new_obj):
                     hit = True
                     del referrer[key]
                     referrer[new_obj] = value
-                                                                        
+
         # LISTS
         elif isinstance(referrer, list):
             for i, value in enumerate(referrer):
                 if value is org_obj:
                     hit = True
                     referrer[i] = new_obj
-        
+
         # SETS
         elif isinstance(referrer, set):
             referrer.remove(org_obj)
@@ -197,36 +197,36 @@ def replace_all_refs(org_obj, new_obj):
                     new_tuple.append(obj)
             replace_all_refs(referrer, type(referrer)(new_tuple))
 
-        # CELLTYPE        
+        # CELLTYPE
         elif isinstance(referrer, _CELLTYPE):
             def proxy0(data):
                 def proxy1(): return data
                 return proxy1
             proxy = proxy0(new_obj)
-            newcell = proxy.func_closure[0]
-            replace_all_refs(referrer, newcell)            
-        
+            newcell = proxy.__closure__[0]
+            replace_all_refs(referrer, newcell)
+
         # FUNCTIONS
         elif isinstance(referrer, _types.FunctionType):
             localsmap = {}
-            for key in ['func_code', 'func_globals', 'func_name', 
-                        'func_defaults', 'func_closure']:
-                orgattr = getattr(referrer, key)
+            for key in ['code', 'globals', 'name',
+                        'defaults', 'closure']:
+                orgattr = getattr(referrer, '__{}__'.format(key))
                 if orgattr is org_obj:
-                    localsmap[key.split('func_')[-1]] = new_obj
+                    localsmap[key] = new_obj
                 else:
-                    localsmap[key.split('func_')[-1]] = orgattr
+                    localsmap[key] = orgattr
             localsmap['argdefs'] = localsmap['defaults']
             del localsmap['defaults']
             newfn = _types.FunctionType(**localsmap)
             replace_all_refs(referrer, newfn)
 
-        # OTHER (IN DEBUG, SEE WHAT IS NOT SUPPORTED). 
+        # OTHER (IN DEBUG, SEE WHAT IS NOT SUPPORTED).
         else:
-            # debug: 
+            # debug:
             # print type(referrer)
             pass
-            
+
     if hit is False:
         raise AttributeError("Object '%r' not found" % org_obj)
 
@@ -237,23 +237,23 @@ _func_code_map = {}
 def _get_self():
 
     global _func_code_map
-    
+
     frame = _inspect.currentframe()
     code = frame.f_back.f_code
     return _func_code_map[code]
 
 
 class _PyjackFunc(object): pass
-        
+
 
 class _PyjackFuncCode(_PyjackFunc):
-            
+
     def __init__(self, fn, proxyfn):
 
         global _func_code_map
-                
+
         self._fn, self._proxyfn = fn, proxyfn
-              
+
         def proxy(*args, **kwargs):
             import pyjack
             self = pyjack._get_self()
@@ -271,33 +271,33 @@ class _PyjackFuncCode(_PyjackFunc):
         value = self._proxyfn(self._fn, *args, **kwargs)
         self._fn.func_code = self._proxy_func_code
         return value
-    
+
     def restore(self):
         self._fn.func_code = self._org_func_code
-        
+
 
 class _PyjackFuncBuiltin(_PyjackFunc):
-        
+
     def __init__(self, fn, proxyfn):
         self._fn = replace_all_refs(fn, self)
         self._proxyfn = proxyfn
 
     def __call__(self, *args, **kwargs):
         return self._proxyfn(self._fn, *args, **kwargs)
-                
+
     def __getattr__(self, attr):
         try:
             return getattr(self._fn, attr)
         except AttributeError:
             bundle = (self._fn, attr,)
             raise AttributeError("function %r has no attr '%s'" % bundle)
-    
+
     def restore(self):
         replace_all_refs(self, self._fn)
-            
+
 
 if __name__ == '__main__':
-    
+
     import doctest
     doctest.testmod(optionflags=524)
-    
+
